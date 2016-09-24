ea06dd119b51018599c429658273ec985442d663
Santeri Rusila <santtu_1993@hotmail.com>
2016-09-24 19:07:20 +0300

    added promise polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+require('es6-promise').polyfill();
+
 
 const mountNode = document.querySelector('.container');
 
